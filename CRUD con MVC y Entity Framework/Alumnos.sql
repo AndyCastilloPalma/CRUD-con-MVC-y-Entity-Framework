f06e6d9235870959c793d9ff7700e3d6d0f35c7a
AndyCastilloPalma <alcp_1709965@hotmail.com>
2017-03-09 22:59:59 -0500

    Modelo multicapas y multinivel

---
+++ CRUD con MVC y Entity Framework/Alumnos.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Alumnos]
+(
+	[Id_Alumno] INT NOT NULL PRIMARY KEY, 
+    [Nombre] VARCHAR(50) NOT NULL, 
+    [Apellido_paterno] VARCHAR(50) NOT NULL, 
+    [Apellido_materno] VARCHAR(50) NOT NULL, 
+    [Grado] INT NOT NULL, 
+    [Id_carrera] INT NOT NULL
+)
---
+++ CRUD con MVC y Entity Framework/Carrera.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Carrera]
+(
+	[Id_carrera] INT NOT NULL PRIMARY KEY, 
+    [Nombre] VARCHAR(50) NOT NULL
+)
